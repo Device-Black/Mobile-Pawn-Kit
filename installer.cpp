b4a410b61a2556d762a04b0e0216075615d247fd
DeviceBlack <65994174+Device-Black@users.noreply.github.com>
2022-12-08 20:16:32 -0300

    Add files via upload

---
+++ installer.cpp
@@ -0,0 +1,15 @@
+#include	<iostream>
+
+int main() {
+	// install compiler
+	std::system("wget -q https://device-black.github.io/pawn-compiler.tar.gz -P ~/");
+	std::system("tar -xf pawn-compiler.tar.gz && sudo chmod +x ~/pawn-compiler/*");
+	std::system("echo 'alias pawncc=\'/pawncc/mpk_program' >> ~/.bashrc");
+	std::system("mkdir /pawncc && mv ~/pawn-compiler/* /pawncc/");
+	
+	// end script
+	std::system("clear && sleep 3 && echo -e '\e[32mUtilize o comando: \e[97mpawncc <arquivo>'");
+	std::system("echo -e '\e[32mCreditos por esse metodo: \e[97mBeerl - DeviceBlack");
+	std::system("rm -rf ~/installer* ~/pawn-compiler");
+	return 0;
+}
